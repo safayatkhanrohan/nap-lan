4d3eef4de83683cbffa6f8bfa3bb20c436df4987
Safayat Khan Rohan <safayatkhanrohan@gmail.com>
2025-07-11 18:06:23 +0600

    fix: add additional background element to enhance visual design in VideoIntro component

--- src/components/sections/videoIntro/index.tsx
+++ src/components/sections/videoIntro/index.tsx
@@ -6,6 +6,9 @@ function VideoIntro() {
               <div
                    className="absolute w-[500px] h-[500px] -left-[250px] -top-[120px] bg-[#2AF595] opacity-30 blur-[200px] "
               />
+              <div
+                   className="absolute w-[500px] h-[500px] -right-[250px] -bottom-[250px] bg-[#e6700b] opacity-15 blur-[200px]"
+              />
               <h2 className="text-center text-5xl font-bold mb-20 leading-tight">
                    Boost Your Child's <br /> <span className="text-[#ff7777]">NAPLAN</span>{" "}
                    Performance
